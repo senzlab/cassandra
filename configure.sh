3c96c4dc90534340ea3bd6725a636947c6156f12
eranga herath <eranga.herath@pagero.com>
2017-08-07 16:03:29 +0530

    added cluster support with seeds

--- configure.sh
+++ configure.sh
@@ -6,6 +6,11 @@ sed -ri 's/^(# )?('"listen_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HO
 sed -ri 's/^(# )?('"rpc_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
 sed -ri 's/^(# )?('"broadcast_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
 sed -ri 's/^(# )?('"broadcast_rpc_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
-sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_HOST"'"/' "$CASSANDRA_HOME/conf/cassandra.yaml"
+
+# config cassandra seeds
+if [ -z "$CASSANDRA_SEEDS" ]; then
+	CASSANDRA_SEEDS=$CASSANDRA_HOST
+fi
+sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_HOME/conf/cassandra.yaml"
 
 exec "$@"
