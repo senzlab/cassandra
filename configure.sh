b58473669f4ed365957db8224452d703fac71264
pagero <erangaeb@gmail.com>
2017-08-12 18:09:32 +0530

    done clustering

--- configure.sh
+++ configure.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 CASSANDRA_HOME=/opt/cassandra
 CASSANDRA_HOST="$(hostname --ip-address)"
+CLUSTER_NAME="senzchain"
 
+sed -ri 's/^(# )?('"cluster_name"':).*/\2 '"$CLUSTER_NAME"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
 sed -ri 's/^(# )?('"listen_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
 sed -ri 's/^(# )?('"rpc_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
-sed -ri 's/^(# )?('"broadcast_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
-sed -ri 's/^(# )?('"broadcast_rpc_address"':).*/\2 '"$CASSANDRA_HOST"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
+
+# config broadcast address
+if [ -z "$CASSANDRA_BROADCAST_ADDRESS" ]; then
+    CASSANDRA_BROADCAST_ADDRESS=$CASSANDRA_HOST
+fi
+sed -ri 's/^(# )?('"broadcast_address"':).*/\2 '"$CASSANDRA_BROADCAST_ADDRESS"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
+sed -ri 's/^(# )?('"broadcast_rpc_address"':).*/\2 '"$CASSANDRA_BROADCAST_ADDRESS"'/' "$CASSANDRA_HOME/conf/cassandra.yaml"
 
 # config cassandra seeds
 if [ -z "$CASSANDRA_SEEDS" ]; then
-	CASSANDRA_SEEDS=$CASSANDRA_HOST
+	CASSANDRA_SEEDS=$CASSANDRA_BROADCAST_ADDRESS
 fi
 sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_HOME/conf/cassandra.yaml"
 
